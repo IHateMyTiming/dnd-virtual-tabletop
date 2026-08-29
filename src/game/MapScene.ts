0c3bea8c4e94a3a887e3873ac33db00820041f11
leonardo <leo.kawachi@gmail.com>
2026-08-29 19:07:37 -0300

    Makin the grid clickable

--- src/game/MapScene.ts
+++ src/game/MapScene.ts
@@ -1,13 +1,28 @@
 import Phaser from "phaser";
+type Terrain = "empty" | "floor" | "wall" | "water";
 
+interface Tile {
+  terrain: Terrain;
+}
+
+const gridSize = 30;
+const cellSize = 24;
 export class MapScene extends Phaser.Scene {
+  private map: Tile[][] = [];
   constructor() {
     super("MapScene");
   }
 
   create() {
-    const gridSize = 30;
-    const cellSize = 24;
+    for (let row = 0; row < gridSize; row++) {
+      this.map[row] = [];
+
+      for (let column = 0; column < gridSize; column++) {
+        this.map[row][column] = {
+          terrain: "empty",
+        };
+      }
+    }
 
     const graphics = this.add.graphics();
 
@@ -21,5 +36,15 @@ export class MapScene extends Phaser.Scene {
         graphics.strokeRect(x, y, cellSize, cellSize);
       }
     }
+    this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+      const column = Math.floor(pointer.x / cellSize);
+      const row = Math.floor(pointer.y / cellSize);
+
+      if (row >= 0 && row < gridSize && column >= 0 && column < gridSize) {
+        this.map[row][column].terrain = "floor";
+
+        console.log(`Changed tile ${column}, ${row} to floor`);
+      }
+    });
   }
 }
